95f21234eeaddd66f21ebb09a1a98b805d42a678
shan3333 <199403ls@gmail.com>
2018-05-12 18:56:45 +0900

    rendering a template:install twig, use render

--- my-project/src/Controller/LuckyController.php
+++ my-project/src/Controller/LuckyController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class LuckyController
+//class LuckyController
+class LuckyController extends Controller
 {
      /**
       * @Route("/lucky/number")
@@ -14,9 +16,12 @@ class LuckyController
     {
         $number = mt_rand(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        //return new Response(
+          //  '<html><body>Lucky number: '.$number.'</body></html>'
+        //);
+        return $this->render('lucky/number.html.twig', array(
+            'number' => $number,
+        ));
     }
 }
 
